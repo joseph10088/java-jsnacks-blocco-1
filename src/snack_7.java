30e0f34289b8f717b2ee16da9ec7d1f5d49a0bc0
joseph <giuseppemanna1998@icloud.com>
2025-07-11 16:55:39 +0200

    snack 7

---
+++ src/snack_7.java
@@ -0,0 +1,10 @@
+public class snack_7 {
+    public static void main(String[] args) {
+    int potenza = 1;
+
+    while(potenza < 1000){
+        potenza *= 2;
+        System.out.println("potenza: " + potenza);
+    }
+    }
+}
